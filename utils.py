33a696e4d6e3c956fd1c772147e54e8ac2d29769
“ewakaaaa” <ewa.suknarowska@gmail.com>
2021-12-07 07:33:20 +0100

    get_test_answer

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+import re
+
 
 def download_test_puzzle(day_number):
     URL = f"https://adventofcode.com/2021/day/{day_number}"
@@ -11,16 +13,34 @@ def download_test_puzzle(day_number):
     with open(f"day{day_number}/puzzle_test.txt", "w") as f:
         f.write(puzzle_test)
 
+def get_test_answer(day_number):
+    URL = f"https://adventofcode.com/2021/day/{day_number}"
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.content, 'html5lib')
+    code_list = str(soup.find_all('code')).split("<code>")
+    for item in code_list:
+        if '<em>' in item:
+            return int(re.search(r'\d+', item).group())
+
 def make_folder(day_number):
     os.mkdir(os.getcwd()+f"/day{day_number}")
-    for step in [1,2]:
+    for step in ["a","b"]:
         with open(f'day{day_number}/day{day_number}_{step}.py', 'w') as f:
             f.write(f"""import os
 import sys
 sys.path.insert(0, os.getcwd()) 
-from utils import read_file
+from utils import read_file, get_test_answer
+from aocd.models import Puzzle
 
-puzzle = read_file({day_number},test = True)"""
+puzzle_test = read_file({day_number},test = True)
+puzzle = Puzzle(year=2021, day={day_number}).input_data
+
+def solution(puzzle):
+    return 
+
+assert solution(puzzle_test) == get_test_answer({day_number})
+puzzle.answer_{step} = solution(puzzle) 
+"""
     )
 
 def read_file(day_number,test = False):
