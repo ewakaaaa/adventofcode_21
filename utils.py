a12ff93913c6c430495f80c22da59c4688f62c20
“ewakaaaa” <ewa.suknarowska@gmail.com>
2021-12-07 07:44:22 +0100

    black

--- utils.py
+++ utils.py
@@ -7,26 +7,39 @@ import re
 def download_test_puzzle(day_number):
     URL = f"https://adventofcode.com/2021/day/{day_number}"
     r = requests.get(URL)
-    soup = BeautifulSoup(r.content, 'html5lib')
-    puzzle_test = soup.find('pre') 
-    puzzle_test = str(puzzle_test).replace("<pre><code>","").replace("</code></pre>","")
+    soup = BeautifulSoup(r.content, "html5lib")
+    puzzle_test = soup.find("pre")
+    puzzle_test = (
+        str(puzzle_test).replace("<pre><code>", "").replace("</code></pre>", "")
+    )
     with open(f"day{day_number}/puzzle_test.txt", "w") as f:
         f.write(puzzle_test)
+
 
 def get_test_answer(day_number):
     URL = f"https://adventofcode.com/2021/day/{day_number}"
     r = requests.get(URL)
-    soup = BeautifulSoup(r.content, 'html5lib')
-    code_list = str(soup.find_all('code')).split("<code>")
+    soup = BeautifulSoup(r.content, "html5lib")
+    code_list = str(soup.find_all("code")).split("<code>")
     for item in code_list:
-        if '<em>' in item:
-            return int(re.search(r'\d+', item).group())
+        if "<em>" in item:
+            return int(re.search(r"\d+", item).group())
+
+
+def read_file(day_number, test=False):
+    file_name = os.getcwd() + f"/day{day_number}/puzzle.txt"
+    if test:
+        file_name = os.getcwd() + f"/day{day_number}/puzzle_test.txt"
+    puzzle = open(file_name, "r")
+    return puzzle.readlines()
+
 
 def make_folder(day_number):
-    os.mkdir(os.getcwd()+f"/day{day_number}")
-    for step in ["a","b"]:
-        with open(f'day{day_number}/day{day_number}_{step}.py', 'w') as f:
-            f.write(f"""import os
+    os.mkdir(os.getcwd() + f"/day{day_number}")
+    for step in ["a", "b"]:
+        with open(f"day{day_number}/day{day_number}_{step}.py", "w") as f:
+            f.write(
+                f"""import os
 import sys
 sys.path.insert(0, os.getcwd()) 
 from utils import read_file, get_test_answer
@@ -41,11 +54,4 @@ def solution(puzzle):
 assert solution(puzzle_test) == get_test_answer({day_number})
 puzzle.answer_{step} = solution(puzzle) 
 """
-    )
-
-def read_file(day_number,test = False):
-    file_name = os.getcwd() + f'/day{day_number}/puzzle.txt'
-    if test: 
-        file_name = os.getcwd() + f'/day{day_number}/puzzle_test.txt'
-    puzzle = open(file_name,'r') 
-    return  puzzle.readlines() 
+            )
